a95e92667a9ab41acb46c6bafc74789028dab71a
Brian McMichael <brian@brianmcmichael.com>
2025-01-27 15:35:00 -0500

    initial commit

--- script/MaseerOne.s.sol
+++ script/MaseerOne.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {MaseerOne} from "../src/MaseerOne.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    MaseerOne public maseerOne;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        maseerOne = new MaseerOne();
 
         vm.stopBroadcast();
     }
---
+++ test/MaseerOne.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MaseerOne} from "../src/MaseerOne.sol";
+
+contract CounterTest is Test {
+    MaseerOne public maseerOne;
+
+    function setUp() public {
+        maseerOne = new MaseerOne();
+        maseerOne.setNumber(0);
+    }
+
+    function test_Increment() public {
+        maseerOne.increment();
+        assertEq(maseerOne.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        maseerOne.setNumber(x);
+        assertEq(maseerOne.number(), x);
+    }
+}
