63070e3f8a7b32638a7a9b4f9777c41a21dfdcc4
Brian McMichael <brian@brianmcmichael.com>
2025-01-27 15:47:29 -0500

    Add forge test script

---
+++ scripts/forge_test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$(cast chain --rpc-url="$ETH_RPC_URL")" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --v=*)     v="${1#*=}"     ;;
+        --mt=*)    mt="${1#*=}"    ;;
+        --mc=*)    mc="${1#*=}"    ;;
+        *) echo "Unknown parameter passed: $1"; exit 1;;
+    esac
+    shift
+done
+
+
+TEST_ARGS=''
+
+vstr="-vv"
+if [ -n "$v" ]; then
+    vstr=""
+    for (( i=0; i<$v; i++ ))
+    do
+        vstr+="v"
+    done
+    if [[ ! -z $vstr ]]; then
+        vstr="-$vstr"
+    fi
+fi
+
+mcstr=""
+if [ -n "$mc" ]; then
+  mcstr="--mc $mc"
+fi
+
+mtstr=""
+if [ -n "$mt" ]; then
+  mtstr="--mt $mt"
+fi
+
+echo "Running ${type} tests"
+test_data="v: ${vstr:-"default"}, mc: ${mc:-"all"}, mt: ${mt:-"all"}"
+
+echo "$test_data"
+
+forge test --fork-url ${ETH_RPC_URL} $mtstr $nmcstr $vstr $mcstr
