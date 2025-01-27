1ffc6f078205ec14f90c1967f3e39ba75dd98211
Brian McMichael <brian@brianmcmichael.com>
2025-01-27 17:52:21 -0500

    test

--- test/MaseerOne.t.sol
+++ test/MaseerOne.t.sol
@@ -8,17 +8,38 @@ contract CounterTest is Test {
     MaseerOne public maseerOne;
 
     function setUp() public {
-        maseerOne = new MaseerOne();
-        maseerOne.setNumber(0);
+        maseerOne = new MaseerOne("MaseerOne", "M1");
     }
 
-    function test_Increment() public {
-        maseerOne.increment();
-        assertEq(maseerOne.number(), 1);
+    function testName() public {
+        assertEq(maseerOne.name(), "MaseerOne");
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        maseerOne.setNumber(x);
-        assertEq(maseerOne.number(), x);
+    function testSymbol() public {
+        assertEq(maseerOne.symbol(), "M1");
+    }
+
+    function testDecimals() public {
+        assertEq(maseerOne.decimals(), 18);
+    }
+
+    function testTotalSupply() public {
+        assertEq(maseerOne.totalSupply(), 0);
+    }
+
+    function testBalanceOf() public {
+        assertEq(maseerOne.balanceOf(address(this)), 0);
+    }
+
+    function testAllowance() public {
+        assertEq(maseerOne.allowance(address(this), address(this)), 0);
+    }
+
+    function testDomainSeparator() public {
+        assertEq(maseerOne.DOMAIN_SEPARATOR(), bytes32(0x5ab14e15c20450fbc9d7535f051e9e913e03b79d61309455cc299c496fecb73b));
+    }
+
+    function testNonces() public {
+        assertEq(maseerOne.nonces(address(this)), 0);
     }
 }
