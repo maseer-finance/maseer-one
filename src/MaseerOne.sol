6fdbc1c39d6e4413524278a324bd01b64e275a83
Brian McMichael <brian@brianmcmichael.com>
2025-01-27 17:52:44 -0500

    Stub out functionality

--- src/MaseerOne.sol
+++ src/MaseerOne.sol
@@ -1,15 +1,94 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-contract MaseerOne {
+import {MaseerToken} from "./MaseerToken.sol";
 
-    uint256 public number;
+contract MaseerOne is MaseerToken {
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) MaseerToken(name_, symbol_) {
+
     }
 
-    function increment() public {
-        number++;
+    function mint(uint256 amt) external {
+        // TODO: Check if the mint is allowed
+
+        // TODO: Oracle price check
+
+        // TODO: Mint the tokens
+
+        // TODO: Emit contract event
     }
+
+    function redeem(uint256 amt) external {
+        // TODO: Check if the burn is allowed
+
+        // TODO: Oracle price check
+
+        // TODO: Add user to the pending redemptions
+
+        // TODO: Burn the tokens
+
+        // TODO: Emit contract event
+
+    }
+
+    function claim() external {
+        // TODO: check if the claim is allowed
+
+        // TODO: Check claimable amount
+
+        // TODO: Check internal balance
+
+        // TODO: Decrement the pending redemptions
+
+        // TODO: Transfer the tokens
+
+        // TODO: Emit claim
+
+    }
+
+
+    // Token overrides for compliance
+
+    function approve(address guy) external override returns (bool) {
+        // TODO: Check if the approval is allowed
+        return super.approve(guy, type(uint256).max);
+    }
+
+    function approve(address guy, uint wad) public override returns (bool) {
+        // TODO: Check if the approval is allowed
+        return super.approve(guy, wad);
+    }
+
+    function transfer(address dst, uint wad) external override returns (bool) {
+        // TODO: Check if the transfer is allowed
+        return super.transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad) public override returns (bool) {
+        // TODO: Check if the transfer is allowed
+        return super.transferFrom(src, dst, wad);
+    }
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public override {
+        // TODO: Check if the transfer is allowed
+        super.permit(owner, spender, value, deadline, v, r, s);
+    }
+
+    // TODO: Oracle price
+    // TODO: Redemption period
+    // TODO: Output conduit
+    // TODO: Pending redemption count
+
 }
--- src/MaseerToken.sol
+++ src/MaseerToken.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-contract MaseerToken {
+abstract contract MaseerToken {
 
-    address                                 immutable public  auth;
+    // ERC-20
     uint256                                           public  totalSupply;
     mapping (address => uint256)                      public  balanceOf;
     mapping (address => mapping (address => uint256)) public  allowance;
     string                                            public  name;
     string                                            public  symbol;
     uint8                                    constant public  decimals = 18; // standard token precision.
-
-    modifier authed() {
-        require(msg.sender == auth, "not-authorized");
-        _;
-    }
+    // EIP-2612
+    bytes32 immutable           public DOMAIN_SEPARATOR;
+    mapping(address => uint256) public nonces;
+    uint256 immutable         internal CHAIN_ID; // token is linked to deployment chain id. Forked tokens are not supported.
 
     constructor(string memory name_, string memory symbol_) {
-        auth   = msg.sender;
         name   = name_;
         symbol = symbol_;
+
+        CHAIN_ID = block.chainid;
+        DOMAIN_SEPARATOR = _domainSeparator();
     }
 
     event Approval(address indexed src, address indexed guy, uint wad);
     event Transfer(address indexed src, address indexed dst, uint wad);
-    event Mint(address indexed guy, uint wad);
-    event Burn(address indexed guy, uint wad);
 
     function approve(address guy) external returns (bool) {
         return approve(guy, type(uint256).max);
@@ -47,56 +46,83 @@ contract MaseerToken {
         public
         returns (bool)
     {
-        if (src != msg.sender && allowance[src][msg.sender] != type(uint256).max) {
-            require(allowance[src][msg.sender] >= wad, "insufficient-approval");
-            allowance[src][msg.sender] = allowance[src][msg.sender] - wad;
+        uint256 _allowance = allowance[src][msg.sender];
+        if (_allowance != type(uint256).max) {
+            allowance[src][msg.sender] = _allowance - wad;
         }
 
-        require(balanceOf[src] >= wad, "insufficient-balance");
-        balanceOf[src] = balanceOf[src] - wad;
-        balanceOf[dst] = balanceOf[dst] + wad;
+        balanceOf[src] -= wad;
+        balanceOf[dst] += wad;
 
         emit Transfer(src, dst, wad);
 
         return true;
     }
 
-    function push(address dst, uint wad) external {
-        transferFrom(msg.sender, dst, wad);
-    }
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8   v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
 
-    function pull(address src, uint wad) external {
-        transferFrom(src, msg.sender, wad);
-    }
+        // Unchecked because the owner's nonce which cannot realistically overflow.
+        unchecked {
+            address recoveredAddress = ecrecover(
+                keccak256(
+                    abi.encodePacked(
+                        "\x19\x01",
+                        DOMAIN_SEPARATOR,
+                        keccak256(
+                            abi.encode(
+                                keccak256(
+                                    "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+                                ),
+                                owner,
+                                spender,
+                                value,
+                                nonces[owner]++,
+                                deadline
+                            )
+                        )
+                    )
+                ),
+                v,
+                r,
+                s
+            );
 
-    function move(address src, address dst, uint wad) external {
-        transferFrom(src, dst, wad);
-    }
+            require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_SIGNER");
 
-
-    function mint(uint wad) external {
-        mint(msg.sender, wad);
-    }
-
-    function burn(uint wad) external {
-        burn(msg.sender, wad);
-    }
-
-    function mint(address guy, uint wad) public authed {
-        balanceOf[guy] = balanceOf[guy] + wad;
-        totalSupply = totalSupply + wad;
-        emit Mint(guy, wad);
-    }
-
-    function burn(address guy, uint wad) public authed {
-        if (guy != msg.sender && allowance[guy][msg.sender] != type(uint256).max) {
-            require(allowance[guy][msg.sender] >= wad, "insufficient-approval");
-            allowance[guy][msg.sender] = allowance[guy][msg.sender] - wad;
+            allowance[recoveredAddress][spender] = value;
         }
 
-        require(balanceOf[guy] >= wad, "insufficient-balance");
-        balanceOf[guy] = balanceOf[guy] - wad;
-        totalSupply = totalSupply - wad;
-        emit Burn(guy, wad);
+        emit Approval(owner, spender, value);
+    }
+
+    function _mint(address guy, uint wad) internal {
+        balanceOf[guy] += wad;
+        totalSupply += wad;
+        emit Transfer(address(0), guy, wad);
+    }
+
+    function _burn(address guy, uint wad) internal {
+        balanceOf[guy] -= wad;
+        totalSupply -= wad;
+        emit Transfer(guy, address(0), wad);
+    }
+
+    function _domainSeparator() internal view returns (bytes32) {
+        return keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes(name)),
+            keccak256(bytes("1")),
+            CHAIN_ID,
+            address(this)
+        ));
     }
 }
