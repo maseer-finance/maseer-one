354cc8a656a9ddaf957be55d0daa0a85522bcae8
Brian McMichael <brian@brianmcmichael.com>
2025-01-28 12:01:00 -0500

    version bump

--- src/MaseerOne.sol
+++ src/MaseerOne.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 interface Gem {
     function transferFrom(address src, address dst, uint wad) external returns (bool);
