69b4d80e0e0fbc5f39b9ed31ec471dc05a85bf28
Brian McMichael <brian@brianmcmichael.com>
2025-01-29 17:02:12 -0500

    Add MaseerPrice

---
+++ src/MaseerPrice.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+contract MaseerPrice {
+
+    mapping (address => uint256) public wards;
+    function rely(address usr) external auth { wards[usr] = 1; }
+    function deny(address usr) external auth { wards[usr] = 0; }
+    modifier auth() {
+        require (wards[msg.sender] == 1, "MaseerPrice/not-authorized");
+        _;
+    }
+
+    constructor() {
+        wards[msg.sender] = 1;
+    }
+
+}
