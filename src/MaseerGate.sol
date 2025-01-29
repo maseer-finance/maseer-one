53f8cc5e9e1d78a348892175154bf3c1f2eb119f
Brian McMichael <brian@brianmcmichael.com>
2025-01-29 16:21:15 -0500

    Add MaseerGate

---
+++ src/MaseerGate.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+contract MaseerGate {
+
+    mapping (address => uint256) public wards;
+    function rely(address guy) external auth { wards[guy] = 1; }
+    function deny(address guy) external auth { wards[guy] = 0; }
+    modifier auth() {
+        require (wards[msg.sender] == 1, "MaseerGate/not-authorized");
+        _;
+    }
+
+    uint256 public open;
+    uint256 public halt;
+
+    function setOpen(uint256 open_) external auth {
+        require(open_ < block.timestamp + 365 days, "MaseerGate/open-too-far");
+        open = open_;
+    }
+
+    function setClose(uint256 halt_) external auth {
+        require(halt_ < block.timestamp + 365 days, "MaseerGate/halt-too-far");
+        halt = halt_;
+    }
+
+    function pauseMarket() external auth {
+        open = 0;
+        halt = 0;
+    }
+
+    function live() external view returns (bool) {
+        return block.timestamp >= open &&
+               block.timestamp <= halt;
+    }
+
+}
+
