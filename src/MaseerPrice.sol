522724b7f6c9401e19261ed48d2c9d04f4d0e04f
Brian McMichael <brian@brianmcmichael.com>
2025-01-30 12:11:58 -0500

    Add upgrade gap

--- src/MaseerPrice.sol
+++ src/MaseerPrice.sol
@@ -5,6 +5,9 @@ contract MaseerPrice {
 
     // Slot 0
     mapping (address => uint256) public wards;
+    // Allocate slots 1-50
+    uint256[49] private __gap;
+
     function rely(address usr) external auth { wards[usr] = 1; }
     function deny(address usr) external auth { wards[usr] = 0; }
     modifier auth() {
