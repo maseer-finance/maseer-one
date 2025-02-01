75d6b06212150ff156cabf29c153eaf743b4a01b
Brian McMichael <brian@brianmcmichael.com>
2025-02-01 13:06:36 -0500

    Upgrade tests

--- test/MaseerOne.proxy.t.sol
+++ test/MaseerOne.proxy.t.sol
@@ -14,9 +14,23 @@ contract MaseerOneProxyTest is MaseerTestBase {
         assertEq(MaseerProxy(maseerOne.pip()).impl(), pipImpl);
     }
 
+    function testUpgradePip() public {
+        address newPipImpl = address(new MaseerPrice());
+        MaseerProxy(pipProxy).file(newPipImpl);
+        assertEq(maseerOne.pip(), pipProxy);
+        assertEq(MaseerProxy(maseerOne.pip()).impl(), newPipImpl);
+    }
+
     function testAct() public view {
         assertEq(maseerOne.act(), actProxy);
         assertEq(MaseerProxy(maseerOne.act()).impl(), actImpl);
+    }
+
+    function testActUpgrade() public {
+        address newActImpl = address(new MaseerGate());
+        MaseerProxy(actProxy).file(newActImpl);
+        assertEq(maseerOne.act(), actProxy);
+        assertEq(MaseerProxy(maseerOne.act()).impl(), newActImpl);
     }
 
     function testCop() public view {
@@ -24,9 +38,23 @@ contract MaseerOneProxyTest is MaseerTestBase {
         assertEq(MaseerProxy(maseerOne.cop()).impl(), copImpl);
     }
 
+    function testCopUpgrade() public {
+        address newCopImpl = address(new MaseerGuard(USDT));
+        MaseerProxy(copProxy).file(newCopImpl);
+        assertEq(maseerOne.cop(), copProxy);
+        assertEq(MaseerProxy(maseerOne.cop()).impl(), newCopImpl);
+    }
+
     function testFlo() public view {
         assertEq(maseerOne.flo(), floProxy);
         assertEq(MaseerProxy(maseerOne.flo()).impl(), floImpl);
+    }
+
+    function testFloUpgrade() public {
+        address newFloImpl = address(new MaseerConduit());
+        MaseerProxy(floProxy).file(newFloImpl);
+        assertEq(maseerOne.flo(), floProxy);
+        assertEq(MaseerProxy(maseerOne.flo()).impl(), newFloImpl);
     }
 
     // TODO: Test proxy interactions
