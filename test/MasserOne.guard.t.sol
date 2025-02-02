4a3b84c1cdf3ff44d82b903c3a378e342fce431e
Brian McMichael <brian@brianmcmichael.com>
2025-02-02 12:05:49 -0500

    comment

--- test/MasserOne.guard.t.sol
+++ test/MasserOne.guard.t.sol
@@ -14,7 +14,7 @@ contract MaseerOneGuardTest is MaseerTestBase {
     }
 
     function testBadActors() public view {
-
+        // Run through the list of bad actors and ensure they are blocked
         uint256 _len = ofac.length();
         for (uint i = 0; i < _len; i++) {
             assertEq(maseerOne.canPass(ofac.ofac(i)), false);
@@ -22,6 +22,7 @@ contract MaseerOneGuardTest is MaseerTestBase {
     }
 
     function testGoodActors() public view {
+        // Ensure that the good actors are allowed to pass
         assertEq(maseerOne.canPass(alice), true);
         assertEq(maseerOne.canPass(bob),   true);
         assertEq(maseerOne.canPass(carol), true);
