e335717f04e7b232c455b8db6784569b9403904c
Brian McMichael <brian@brianmcmichael.com>
2025-02-03 00:52:06 -0500

    Add oracle tests

--- test/MaseerOne.price.t.sol
+++ test/MaseerOne.price.t.sol
@@ -9,5 +9,48 @@ contract MaseerOnePriceTest is MaseerTestBase {
 
     }
 
-    // TODO: Test oracle operations
+    function testPriceName() public {
+        assertEq(pip.name(), "");
+
+        vm.prank(pipAuth);
+        pip.file("name", "CANAUSDT");
+
+        assertEq(pip.name(), "CANAUSDT");
+    }
+
+    function testPriceRead() public {
+        assertEq(pip.read(), 0);
+
+        // Example where price is $30.24
+        // 1 CANA = 30.24 USDT
+        // 30.24 * 10^6 = 30240000
+        vm.prank(pipAuth);
+        pip.poke(30240000);
+
+        assertEq(pip.read(), 30240000);
+    }
+
+    function testFuzzPriceUpdate(uint256 _price, uint256 _rand) public {
+
+        bool _ok = (_rand % 2 == 0);
+
+        if (!_ok) {
+            vm.expectRevert("MaseerPrice/not-authorized");
+            pip.poke(_price);
+            return;
+        }
+
+        vm.prank(pipAuth);
+        pip.poke(_price);
+
+        assertEq(pip.read(), _price);
+    }
+
+    function testFuzzNameUpdate(bytes32 _name) public {
+
+        vm.prank(pipAuth);
+        pip.file("name", _name);
+
+        assertEq(pip.name(), _bytes32toString(_name));
+    }
 }
