cbc7ab2f6b5520ea961c0327d1580da5aa413fdd
Brian McMichael <brian@brianmcmichael.com>
2025-04-07 16:16:21 -0400

    Add BUSL license headers

--- src/MaseerImplementation.sol
+++ src/MaseerImplementation.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: BUSL-1.1
+// Copyright (c) 2025 Maseer LTD
+//
+// This file is subject to the Business Source License 1.1.
+// You may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at:
+// https://github.com/Maseer-LTD/maseer-one/blob/master/LICENSE
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 pragma solidity ^0.8.28;
 
 abstract contract MaseerImplementation {
